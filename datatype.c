88f88ee9400eb7d082e00ccafb7112bf6a71f473
GauravKumarChaurasiya <2002gauravkumarchaurasiya@gmail.com>
2022-02-17 11:26:15 +0530

    datatype and input added

---
+++ datatype.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+    char c = 'g';
+    short a =45 ;
+    int b = 458;
+    long d = 545644;
+    float e = 450.36f;
+    double f = 26562.266;
+    getch();
+
+}
---
+++ input.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+    int a;
+   // clrscr();
+   printf("Enter a number : ");
+   scanf("%d",&a);
+   printf("The number you enter is %d",a);
+   getch();
+
+}
