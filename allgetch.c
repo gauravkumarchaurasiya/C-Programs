2a932e8194abf9ef61a06065b3e0ae9e905ceb05
GauravKumarChaurasiya <2002gauravkumarchaurasiya@gmail.com>
2022-02-25 22:29:22 +0530

    all getch function added

---
+++ allgetch.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    int a,b,c;
+    //clrscr();
+    printf("Enter value of a in getch ");
+    a = getch();
+    printf("Enter value of a in getche ");
+    b = getche();
+    printf("Enter value of a in getchar ");
+    c = getchar();
+    printf("%c,%c,%c ",a,b,c);
+
+}
