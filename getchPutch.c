fb15c757c29ad44d02d78d2fafaa863042a7b2e0
GauravKumarChaurasiya <2002gauravkumarchaurasiya@gmail.com>
2022-02-25 22:31:15 +0530

    getch and putch function file added

---
+++ getchPutch.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+    int ch;
+    //clrscr();
+    printf("\nEnter a character");
+    ch = getch();
+    printf("\nThe Character enter by you ");
+    putch(ch);
+    getch();
+}
