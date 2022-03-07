08e145ed32817eabbb26865c03f90789c54dfbe4
Gauravkumarchaurasiya <2002gauravkumarchaurasiya@gmail.com>
2022-03-07 23:52:31 +0530

    factorial fiel added

---
+++ factorial.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    int n=5,fact=0;
+    //clrscr();
+    for (int i = 1;i<=n;i++){
+        fact*=i;
+
+
+
+    }
+    printf("the factorial of %d is : %d",n,fact);
+
+}
